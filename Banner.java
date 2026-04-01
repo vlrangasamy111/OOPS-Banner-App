748aab681e5fd361c38c6526c6e08febf0a8dd7b
Rangasamy V L <vlrangasamy111@gmail.com>
2026-04-01 21:41:28 +0530

    Add Banner class to display a banner pattern

---
+++ Banner.java
@@ -0,0 +1,12 @@
+public class Banner {
+
+    public static void main(String[] args) {
+        System.out.println(" *****    *****    ******    ***** ");
+        System.out.println("*     *  *     *   *     *  *     *");
+        System.out.println("*     *  *     *   *     *  *      ");
+        System.out.println("*     *  *     *   ******    ***** ");
+        System.out.println("*     *  *     *   *              *");
+        System.out.println("*     *  *     *   *        *     *");
+        System.out.println(" *****    *****    *         ***** ");
+    }
+}
